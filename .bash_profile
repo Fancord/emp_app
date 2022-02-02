efd752c928a6ac1faa94dae47a5e2fdf96f53d5d
Fancord <gidgreed@gmail.com>
2022-02-02 21:44:39 +0300

    some_change

--- .bash_profile
+++
@@ -1,1 +0,0 @@
-export SECRET_KEY_EMP_APP="django-insecure-^i0#=0g%(*&x=%@am$mwbtkm0z=+(&ij5rx5rj-gxj=u(s3!26";
--- emp_app/settings.py
+++ emp_app/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^i0#=0g%(*&x=%@am$mwbtkm0z=+(&ij5rx5rj-gxj=u(s3!26"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1',]
 
 
 # Application definition
